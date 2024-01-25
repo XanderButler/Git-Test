e1a4765ca2cd7b6110ae92bb68a4c68ed59e47c9
Xander Butler <xbutler@ut.utm.edu>
2024-01-25 15:57:25 -0600

    New main.cpp, now with addinggit add main.cpp!

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
+int sum(int n) {
+    int result = 0;
+    for (int i = 1; i <= n; ++i) {
+        result += i;
+    }
+    return result;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    
+    std::cout << "Enter a number, n: ";
+    
+    int n;
+    std::cin >> n;
+
+    std::cout << "Sum of numbers from 1 to " << n << ": " << sum(n) << std::endl;
 
     return 0;
 }
